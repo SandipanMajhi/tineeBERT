47abf20a927c3705e664f70e163f5fb26c4e4335
SandipanMajhi <sandipan.majhi.email@gmail.com>
2025-03-31 02:37:39 +0530

    BERT coded

--- Model/bert.py
+++ Model/bert.py
@@ -1,7 +1,36 @@
 import torch
 import torch.nn as nn
 
+from Model.blocks import EmbeddingLayer, SinusoidPE, MultiheadAttention, FeedForward, EncoderLayer
 
-class BertModel(nn.Module):
-    def __init__(self):
+
+class TineeBERT(nn.Module):
+    def __init__(self, num_repeats, vocab_size, embed_size, seqlen, num_heads, device = "cpu"):
         super().__init__()
+
+        self.device = device
+        self.embed_size = embed_size
+        self.num_repeats = num_repeats
+        self.vocab_size = vocab_size
+        self.seqlen = seqlen
+        self.num_heads = num_heads
+
+        self.embedding = EmbeddingLayer(vocab_size, embed_size)
+        self.positional_encoding = SinusoidPE(embed_size, seqlen, device = device)
+        
+        self.encoders = nn.ModuleList([
+            EncoderLayer(embed_size, num_heads = num_heads, device = device) for _ in range(num_repeats)
+        ]) 
+
+
+    def forward(self, x, mask = None):
+        x = self.embedding(x)
+        x = self.positional_encoding(x)
+
+        for encoder in self.encoders:
+            x = encoder(x, mask = mask)
+
+        return x
+
+
+
---
+++ utils/tineeTrainer.py
@@ -0,0 +1,7 @@
+from Model.bert import TineeBERT
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
